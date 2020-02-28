35a7bc056a301c8171d4910d5c87dd3afce17e3c
Anirudh Prakash <anipra7@gmail.com>
2020-02-27 21:57:10 -0800

    Added code to draw line graph for all csvs
    all drawn on the one graph, with colors chosen randomly

--- src/index.js
+++ src/index.js
@@ -29,11 +29,28 @@ d3.csv('carbon-emissions.csv')
   })
 
 ////////////////////////////////////////////////////////////////////////////////
+const FILE_PATH = "news_topics_2019/";
+
+var newsTopicTerms = ["Area 51 raid", "Baby Yoda", "Boeing 737 crashes",
+  "California earthquake", "California wildfires", "Christchurch shooting",
+  "Coco Gauff", "College Football Playoff", "Dayton shooting",
+  "El Paso shooting", "Equifax data breach", "FIFA Women's World Cup",
+  "government shutdown", "Greta Thunberg", "Hurricane Dorian", "Katelyn Ohashi",
+  "Lori Loughlin college scandal", "MLS Cup", "Muller Report",
+  "NCAA Men's Division I Basketball Tournament", "Notre Dame fire",
+  "Stanley Cup", "Super Bowl LIII", "The NBA Finals", "Tiger Woods Masters",
+  "Trump impeachment", "vaping", "World Series"];
+
+var newsTopicFiles = [];
+newsTopicTerms.forEach(function (topic) {
+  newsTopicFiles.push(topic.replace(/ /g, "_") + ".csv");
+});
+//console.log(newsTopicFiles);
 
 // Code to draw simple line graph of one of the csv files //
 
 const WIDTH = 700;
-const HEIGHT = 350;
+const HEIGHT = WIDTH / 2;
 
 // Set the dimensions of the canvas / graph
 var margin = { top: 20, right: 20, bottom: 50, left: 50 },
@@ -64,36 +81,49 @@ var svg = d3.select("#graph")
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// Get the data
-d3.csv("news_topics_2019/Area_51_raid.csv")
-  .then((data) => {
-    //console.log(data);
-    data.forEach(function (d) {
-      d.Week = parseDate(d.Week);
-      d.interest = +d.interest;
-      if (isNaN(d.interest)) {
-        d.interest = 0;
-      }
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function drawLine(fileName) {
+  // Get the data
+  d3.csv(FILE_PATH + fileName)
+    .then((data) => {
+      //console.log(data);
+      data.forEach(function (d) {
+        d.Week = parseDate(d.Week);
+        d.interest = +d.interest;
+        if (isNaN(d.interest)) {
+          d.interest = 0;
+        }
+      });
+
+      // Scale the range of the data
+      x.domain(d3.extent(data, function (d) { return d.Week; }));
+      y.domain([0, d3.max(data, function (d) { return d.interest; })]);
+
+      // Add the valueline path.
+      svg.append("path")
+        .attr("class", "line")
+        .style("stroke", getRandomColor() /*d3.scaleSequential(d3["interpolateRainbow"])*/)
+        .attr("d", valueline(data));
+
+      // Add the X Axis
+      svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+      // Add the Y Axis
+      svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
     });
+}
 
-    // Scale the range of the data
-    x.domain(d3.extent(data, function (d) { return d.Week; }));
-    y.domain([0, d3.max(data, function (d) { return d.interest; })]);
-
-    // Add the valueline path.
-    svg.append("path")
-      .attr("class", "line")
-      .attr("d", valueline(data));
-
-    // Add the X Axis
-    svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-
-    // Add the Y Axis
-    svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis);
-  });
-
+newsTopicFiles.forEach(function (fileName) { drawLine(fileName) });
