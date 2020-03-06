25dbab18fb405f4de03e2d701752f24d7a1d2a23
Zachary <zkeyes@cs.washington.edu>
2020-03-06 13:04:45 -0800

    Update docs

--- docs/nyt_articles.fa66dbf6.js
+++ docs/nyt_articles.fa66dbf6.js
@@ -28895,6 +28895,7 @@ Object.keys(_d3Zoom).forEach(function (key) {
 var d3 = require('d3');
 
 var newsTopicTerms = ['Area 51 raid', 'Baby Yoda', 'Boeing 737 crashes', 'California earthquake', 'California wildfires', 'Christchurch shooting', 'Coco Gauff', 'College Football Playoff', 'Dayton shooting', 'El Paso shooting', 'Equifax data breach', 'FIFA Women\'s World Cup', 'government shutdown', 'Greta Thunberg', 'Hurricane Dorian', 'Katelyn Ohashi', 'Lori Loughlin college scandal', 'MLS Cup', 'Muller Report', 'NCAA Men\'s Division I Basketball Tournament', 'Notre Dame fire', 'Stanley Cup', 'Super Bowl LIII', 'The NBA Finals', 'Tiger Woods Masters', 'Trump impeachment', 'vaping', 'World Series'];
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 var nytTopicFiles = [];
 newsTopicTerms.forEach(function (topic) {
   nytTopicFiles.push('nyt_articles/NYT_' + topic.replace(/ /g, '_') + '.json');
@@ -28903,7 +28904,7 @@ newsTopicTerms.forEach(function (topic) {
 //     console.log(response)
 // }).catch((error) => { console.error(error) })
 
-d3.json(nytTopicFiles[4]).then(function (data) {
+d3.json(nytTopicFiles[18]).then(function (data) {
   var articles = [];
 
   for (var article in data.articles) {
@@ -28915,29 +28916,44 @@ d3.json(nytTopicFiles[4]).then(function (data) {
     d2 = new Date(b.pub_date).getTime();
     return d2 - d1;
   });
-  var svg = d3.select('#nyt_articles').append('svg').attr('id', 'scroll-svg').attr('height', 155 * articles.length + 35 + 'px');
+  var svg = d3.select('#nyt_articles').append('svg').attr('id', 'scroll-svg').attr('height', 155 * articles.length + 40 + 'px');
   var y = d3.scaleLinear().range([0, 155]);
   var i = 0; // draw the rects
-
-  svg.selectAll('rect').data(articles).enter().append('rect').attr('y', function () {
-    return y(i++) + 5;
-  }).attr('height', 150).attr('width', '100%').style('fill', 'lightgrey').style('opacity', 1.0); // draw the nyt titles
+  // svg.selectAll('rect')
+  //     .data(articles)
+  //     .enter()
+  //     .append('rect')
+  //     .attr('y', () => y(i++) + 5 )
+  //     .attr('height', 150)
+  //     .attr('width', '100%')
+  //     .style('fill', 'lightgrey')
+  //     .style('opacity', 1.0)
+  // draw the nyt titles
 
   i = 0;
-  svg.selectAll('a').data(articles).enter().append('a').attr('href', function (d) {
+  svg.selectAll('#headline-link').data(articles).enter().append('a').attr('id', 'headline-link').attr('href', function (d) {
     return d.web_url;
-  }).attr('target', '_blank').append('text').style('font-size', '20px').attr('fill', 'black').attr('x', 15).attr('y', function () {
-    return y(i++) + 85;
+  }).attr('target', '_blank').append('text').attr('id', 'headline').attr('x', '195').attr('y', function () {
+    return y(i++) + 80;
   }).attr('text-anchor', 'left').text(function (d) {
     return d.headline.main;
   });
-  var j = 0;
-  svg.selectAll('image').data(articles).enter().append('image').attr('href', function (d) {
-    return d.multimedia.length > 0 ? 'https://www.nytimes.com/' + d.multimedia[0].url : './nyt_articles/nyt_logo.png';
-  }).attr('width', '150').attr('height', '150').attr('x', '800').attr('y', function () {
-    return y(j++) + 5;
+  i = 0;
+  svg.selectAll('#date').data(articles).enter().append('text').attr('id', 'date').attr('x', '195').attr('y', function () {
+    return y(i++) + 100;
+  }).attr('text-anchor', 'left').text(function (d) {
+    var date = new Date(d.pub_date);
+    return months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
   });
-  svg.append('a').attr('href', 'https://developer.nytimes.com').attr('target', '_blank').append('image').attr('href', './nyt_articles/nyt_api_logo65.png').attr('x', '0').attr('y', 155 * articles.length + 5);
+  i = 0;
+  svg.selectAll('#image-link').data(articles).enter().append('a').attr('id', 'image-link').attr('href', function (d) {
+    return d.web_url;
+  }).attr('target', '_blank').append('image').attr('href', function (d) {
+    return d.multimedia.length > 0 ? 'https://www.nytimes.com/' + d.multimedia[0].url : './nyt_articles/nyt_logo.png';
+  }).attr('width', '155').attr('height', '155').attr('x', '20').attr('y', function () {
+    return y(i++) + 5;
+  });
+  svg.append('a').attr('id', 'nyt-api-attribution').attr('href', 'https://developer.nytimes.com').attr('target', '_blank').append('image').attr('href', './nyt_articles/nyt_api_logo.png').attr('x', '0').attr('y', 155 * articles.length + 5);
 });
 },{"d3":"../node_modules/d3/index.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
@@ -28967,7 +28983,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61920" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62460" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- docs/src.e31bb0bc.js
+++ docs/src.e31bb0bc.js
@@ -29066,7 +29066,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61920" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62460" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
