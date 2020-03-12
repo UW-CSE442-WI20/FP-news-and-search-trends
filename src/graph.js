43ee8c0015bd76426c855a7824a0f6a1a82b18cf
Anirudh Prakash <anipra7@gmail.com>
2020-03-11 23:44:57 -0700

    added Enter button for search
    can either hit Enter key (already implemented) or hit Enter button on screen (newly implemented)

--- src/graph.js
+++ src/graph.js
@@ -211,8 +211,15 @@ var input = document.getElementById('searchbar');
 input.addEventListener("keyup", event => {
   if (event.keyCode === 13) {
     event.preventDefault();
+    //console.log("key searched");
     search();
   }
+});
+
+var enterButton = document.getElementById('searchbutton');
+enterButton.addEventListener("click", () => {
+  //console.log("button searched");
+  search();
 });
 
 function updateTime(event) {
