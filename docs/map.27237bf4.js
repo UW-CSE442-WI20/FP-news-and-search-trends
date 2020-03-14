79d71e88a60fc2fb0c82c12a42f238a3ed5c7892
alexism9 <alexism9@cs.washington.edu>
2020-03-13 23:58:28 -0700

    final

--- docs/map.27237bf4.js
+++ docs/map.27237bf4.js
@@ -29184,7 +29184,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62709" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63708" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
